6bd2d5362d1df242e8d5e24ac4e2c59d92f3f652
Pontus Sundgren <sundgren.pontus@gmail.com>
2023-08-27 00:13:39 +0200

    Idk what this does but it fixes things

--- src/views/sections/CreativeCatalogue.jsx
+++ src/views/sections/CreativeCatalogue.jsx
@@ -4,9 +4,9 @@ import React from "react";
 import Section from "../../components/common/Section";
 
 /* Project image */
-import placeholder_img from "../../assets/images/project_images/placeholder_img.jpg";
+import placeholder_project_image from "../../assets/images/project_images/placeholder_project_image.jpg";
 import wp_tech_image from "../../assets/images/project_images/wp_tech.jpeg";
-import time_logger_image from "../../assets/images/project_images/time_logger_image.jpg";
+import time_logger_image from "../../assets/images/project_images/time_logger.jpeg";
 
 //TODO: Fix these images
 const stormer_image = null;
@@ -15,29 +15,34 @@ const twiceme_image = null;
 const PROJECT_LIST = [
   {
     id: "2",
-    title: "A fresh look - Tech agency complete rebranding",
-    desc: "A complete rebranding of a full-solution tech agency. The project covered setting up a visual identity for the agency, logo & graphical design, UX/UI design for a new website.",
+    heading: "A fresh look",
+    subHeading: "Tech agency complete rebranding",
+    desc: "A complete rebranding of a full-solution tech agency. The project covered setting up a visual identity for the agency, logo and graphical design, UX/UI design for a new website.",
     to: "/",
     styling: "mt-28",
   },
   {
     id: "1",
-    title: "Time logger - An app log & manage your work hours",
-    desc: "With capability gaps with external products, a company decided to develop the product themselves. An app for employees to log and manage their work hours, sick leave, & vacation.",
+    heading: "Time logger",
+    subHeading:
+      "An app for to allow employees to log and manage your work hours",
+    desc: "With capability gaps with external products, a company decided to develop the product themselves. An app for employees to log and manage their work hours, sick leave, and vacation.",
     to: "/",
     styling: "mt-0",
   },
   {
     id: "4",
-    title: "Twiceme Technology - transforming brand and strategy for tomorrow ",
-    desc: "Diving deep into stakeholders demands & user behaviour, a strategy a personal safety company was created to ",
+    heading: "Twiceme Technology",
+    subHeading: "Shaping a brand and strategy for the future",
+    desc: "Diving deep into stakeholders demands and user behaviour, a strategy a personal safety company was created to ",
     to: "/",
     styling: "mt-28",
   },
   {
     id: "3",
-    title: "Stormer - a brainstorming tool for aspiring designer & developers",
-    desc: "A website that generates lifelike app concept, echoing the dynamics of authentic projects. To mirror real-world client demands & to allow aspiring designers & developers to gain practical insight of their liking.",
+    heading: "Stormer",
+    subHeading: "A brainstorming tool for aspiring designers and developers",
+    desc: "A website that generates lifelike app concept, echoing the dynamics of authentic projects. To mirror real-world client demands and to allow aspiring designers and developers to gain practical insight of their liking.",
     to: "/",
     styling: "mt-0",
   },
@@ -60,10 +65,11 @@ const CreativeCatalogue = () => {
         </div>
         <ul className="grid grid-cols-1 lg:grid-cols-2 gap-x-14 gap-y-14">
           {PROJECT_LIST.map((entry) => (
-            <div key={entry.title}>
+            <div key={entry.heading}>
               <ProjectCard
                 id={entry.id}
-                title={entry.title}
+                heading={entry.heading}
+                subHeading={entry.subHeading}
                 desc={entry.desc}
                 to={entry.to}
                 styling={entry.styling}
@@ -80,7 +86,7 @@ const CreativeCatalogue = () => {
  * This components reders a card for each projects that's added to the array PROJECT_LIST in this file
  */
 
-const ProjectCard = ({ id, title, desc, to, styling }) => {
+const ProjectCard = ({ id, heading, subHeading, desc, to, styling }) => {
   const { innerWidth: width } = window;
   return (
     <li
@@ -91,7 +97,11 @@ const ProjectCard = ({ id, title, desc, to, styling }) => {
       <div className="flex flex-col space-y-6 group">
         <ProjectImage id={id} />
         <div className="flex flex-col space-y-3">
-          <h1 className="text-xl font-primary mt-6">{title}</h1>
+          <div className="flex flex-row space-x-6">
+            <div className="flex flex-row items-center space-x-3 mt-6 text-xl font-primary">
+              <h1 className="text-xl font-primary">{`${heading} - ${subHeading}`}</h1>
+            </div>
+          </div>
           <p className="text-gray-600 lg:text-gray-300 group-hover:text-gray-600 transition-all duration-500 ease-out">
             {desc}
           </p>
@@ -106,7 +116,7 @@ const ProjectCard = ({ id, title, desc, to, styling }) => {
  */
 
 const ProjectImage = ({ id }) => {
-  const img_container = "relative overflow-hidden";
+  const img_container = "relative overflow-hidden shadow-md";
   const img_styling =
     "aspect-auto h-full w-full object-cover object-left-top transition-all duration-500 ease-out group-hover:scale-105";
   switch (id) {
@@ -114,7 +124,9 @@ const ProjectImage = ({ id }) => {
       return (
         <div className={img_container}>
           <img
-            src={time_logger_image ? time_logger_image : placeholder_img}
+            src={
+              time_logger_image ? time_logger_image : placeholder_project_image
+            }
             alt="Time logging app project"
             className={time_logger_image ? img_styling : null}
           />
@@ -124,7 +136,7 @@ const ProjectImage = ({ id }) => {
       return (
         <div className={img_container}>
           <img
-            src={wp_tech_image ? wp_tech_image : placeholder_img}
+            src={wp_tech_image ? wp_tech_image : placeholder_project_image}
             alt="Tech angency rebranding project"
             className={wp_tech_image ? img_styling : null}
           />
@@ -134,7 +146,7 @@ const ProjectImage = ({ id }) => {
       return (
         <div className={img_container}>
           <img
-            src={stormer_image ? stormer_image : placeholder_img}
+            src={stormer_image ? stormer_image : placeholder_project_image}
             alt="App brainstorming tool project"
             className={stormer_image ? img_styling : null}
           />
@@ -144,7 +156,7 @@ const ProjectImage = ({ id }) => {
       return (
         <div className={img_container}>
           <img
-            src={twiceme_image ? twiceme_image : placeholder_img}
+            src={twiceme_image ? twiceme_image : placeholder_project_image}
             alt="Tech angency rebranding project"
             className={twiceme_image ? img_styling : null}
           />
@@ -154,7 +166,7 @@ const ProjectImage = ({ id }) => {
       return (
         <div className={img_container}>
           <img
-            src={placeholder_img}
+            src={placeholder_project_image}
             alt="Could not find presentation file"
             className={img_styling}
           />
