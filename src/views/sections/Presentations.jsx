8a1ce37d01317431fe7fe9e7f81b9f5210625032
Pontus Sundgren <sundgren.pontus@gmail.com>
2024-01-03 12:41:07 +0100

    Adds mockups for WP appen

--- src/views/sections/Presentations.jsx
+++ src/views/sections/Presentations.jsx
@@ -5,13 +5,22 @@ import { Link } from "react-router-dom";
 import Section from "../../components/common/Section";
 import SVG from "../../assets/svgs/SVG";
 import { WPTechLogo } from "../../components/common/Logo";
-import { RevealEqualInView } from "../../components/common/Reveal";
+import {
+  RevealEqualInView,
+  RevealStaggeredInView,
+} from "../../components/common/Reveal";
 
 /* WP TECH MOCKUP IMAGES */
 import wp_tech_start from "../../assets/images/wp_tech_start.jpg";
 
 /* WP APPEN MOCKUP IMAGES */
 import wp_appen_worker from "../../assets/images/project_images/wp_appen/worker_mockup.jpeg";
+import phone_one from "../../assets/images/project_images/wp_appen/phone_1_side.png";
+import phone_two from "../../assets/images/project_images/wp_appen/phone_2_side.png";
+import phone_three from "../../assets/images/project_images/wp_appen/phone_3_side.png";
+import phone_four from "../../assets/images/project_images/wp_appen/phone_4_side.png";
+import phone_five from "../../assets/images/project_images/wp_appen/phone_5_side.png";
+import phone_six from "../../assets/images/project_images/wp_appen/phone_6_side.png";
 
 /* STORMER MOCKUP IMAGES */
 import stormer_start from "../../assets/images/stormer_start.jpg";
@@ -258,11 +267,116 @@ export const MockupsPresentation = ({ type }) => {
       return (
         <div className="pb-36">
           <div className="space-y-6">
-            <Section>
-              <h1 className="text-neutral-500 text-sm">Result</h1>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row gap-y-6 lg:justify-between items-center">
+                <img src={phone_one} alt="Phone mockup" className="w-[650px]" />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Reporting availability
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    Field workers are able to report whenever they are available
+                    to work. This report is then sent to a planning department
+                    that match available field workers to requested services
+                    from customers.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
+            </Section>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row-reverse gap-y-6 lg:justify-between items-center">
+                <img src={phone_two} alt="Phone mockup" className="w-[650px]" />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Get an overview of your jobs
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    See the jobs you have planned for the day or select a later
+                    date to check. See the current status of the different jobs
+                    and get notified if a co-worker have added information to
+                    the job.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
+            </Section>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row gap-y-6 lg:justify-between items-center">
+                <img
+                  src={phone_three}
+                  alt="Phone mockup"
+                  className="w-[650px]"
+                />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Read and confirm job information
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    Inside the job you'll find everything you need. All the
+                    information that is registered by the planning department is
+                    visible inside the information tab. Confirm important
+                    information to let the planning department how the job is
+                    coming along.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
+            </Section>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row-reverse gap-y-6 lg:justify-between items-center">
+                <img
+                  src={phone_four}
+                  alt="Phone mockup"
+                  className="w-[650px]"
+                />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Report your working hours
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    No need for tedious spreadsheets. Report your working hours
+                    once you've marked the job as complete and get timestamps
+                    and suggested values instead of having to keep track of time
+                    while you're focused on the job.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
+            </Section>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row gap-y-6 lg:justify-between items-center">
+                <img
+                  src={phone_five}
+                  alt="Phone mockup"
+                  className="w-[650px]"
+                />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Add notes and comments for your co-workers
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    Add important information to the job that your co-workers
+                    will be notified about. Also take part of important events
+                    that always keeps you up to date on the progress of the job.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
+            </Section>
+            <Section p="pb-36 gap-y-14">
+              <div className="flex flex-col lg:flex-row-reverse gap-y-6 lg:justify-between items-center">
+                <img src={phone_six} alt="Phone mockup" className="w-[650px]" />
+                <RevealStaggeredInView motionClass="space-y-6">
+                  <h1 className="text-neutral-500 text-sm">
+                    Handle payment from the app
+                  </h1>
+                  <p className="text-neutral-700 text-2xl max-w-[700px]">
+                    Get an auto generated invoice based on you and your
+                    co-workers reported hours. Presentable to the customer on
+                    site. Send the invoice to the customer by a simple press of
+                    a button.
+                  </p>
+                </RevealStaggeredInView>
+              </div>
             </Section>
             <RevealEqualInView>
-              <Section>
+              <Section p="pb-36">
                 <div className="flex flex-row justify-between">
                   <img
                     src={wp_appen_worker}
