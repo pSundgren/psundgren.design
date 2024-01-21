a7b1efec912eeef2a8bfa02aba5ddb471153c639
Pontus Sundgren <sundgren.pontus@gmail.com>
2024-01-21 16:56:30 +0100

    Fix text

--- src/views/sections/About.jsx
+++ src/views/sections/About.jsx
@@ -9,12 +9,10 @@ const About = ({ changeCursorVariant }) => {
       <RevealStaggeredInView motionClass={"text-3xl space-y-6 max-w-[900px]"}>
         <h2 className="text-sm text-gray-500">Hello there,</h2>
         <p className="text-2xl text-neutral-700">
-          I'm <span>Pontus Sundgren</span>, a dynamic product designer
-          and front-end developer based in Stockholm. Currently
-          leading the product design for two impactful applications. I bring a diligent approach honed during my
-          master's studies in interaction technology and design.
-        </p>
-        <p className="text-2xl text-neutral-700">
+          I'm <span>Pontus Sundgren</span>, a dynamic product designer and
+          front-end developer based in Stockholm. Currently leading the product
+          design for two impactful applications. I bring a diligent approach
+          honed during my master's studies in interaction technology and design.
           I thrive on diving deep into a product, collaborating seamlessly with
           teams to enhance and optimize every aspect. As a detail-oriented
           designer, I find joy in the iterative journey of constant improvement,
